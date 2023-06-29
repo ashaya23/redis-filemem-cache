784e40c30b5f9019ee744bc6c4c8043d8ffc24e1
ashaya23 <ashay.agarwal@gmail.com>
2023-06-29 07:58:05 -0400

    Update fileMemCache.py

--- filememcache/fileMemCache.py
+++ filememcache/fileMemCache.py
@@ -5,18 +5,20 @@ Created on May 2, 2019
 
 
    Redis and File cache for storing functions indexed by datetime
+   import datetime as dt
    
    cache = FileMemCache(namespace="pycache1",filecache=r'z:\cache' )
 
     @cache.cache_it()
     def test(dateDt, a,b):
-        return a+b
+        if dateDt >= dt.date(2000,3,1):
+           return (a+b) * dateDt.month()
+         else:
+            return a*b  * dateDt.year()
 
 
-    start_date = datetime.date(2000,1,1)
-
-
-    test(start_date, 10, 20)
+    test(datetime.date(2000,1,1), 10, 20)
+    test(datetime.date(2005,1,1), 10, 20)
                 
     cache.list_memory()
     cache.list_files()
